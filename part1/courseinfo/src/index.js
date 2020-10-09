7abc5e2c1a0b6ced52ee46493e5d6ecd7d128a7a
alim0x <alim0x@outlook.com>
2020-10-09 22:44:09 +0800

    complete Exercises 1.3.-1.5.

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,35 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part = ['Fundamentals of React', 'Using props to pass data', 'State of a component']
-  const exercises = [10, 7, 14]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part} exercises={exercises}/>
-      <Total total={exercises[0] + exercises[1] + exercises[2]}/>
+      <Content course={course}/>
+      <Total course={course}/>
     </div>
   )
 }
 
 const Header = (props) => (
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
 )
 
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part[0]} exercises={props.exercises[0]}/>
-      <Part part={props.part[1]} exercises={props.exercises[1]}/>
-      <Part part={props.part[2]} exercises={props.exercises[2]}/>
+      <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises}/>
+      <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises}/>
+      <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises}/>
     </>
   )
 }
 
 const Total = (props) => (
-  <p>Number of exercises {props.total}</p>
+  <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
 )
 
 const Part = (props) => {
