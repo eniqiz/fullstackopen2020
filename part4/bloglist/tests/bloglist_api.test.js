c6eedb99bf26cee099fb1f32c4f35d2376a4e70a
Mo <alim0x@outlook.com>
2021-01-26 17:15:13 +0800

    complete Exercises 4.9.-4.10.

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -1,49 +1,18 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const helper = require('./test_helper')
 
 const api = supertest(app)
 
 const Blog = require('../models/blog')
 
-const initialBlogs = [{
-  title: 'React patterns',
-  author: 'Michael Chan',
-  url: 'https://reactpatterns.com/',
-  likes: 7
-}, {
-  title: 'Go To Statement Considered Harmful',
-  author: 'Edsger W. Dijkstra',
-  url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-  likes: 5
-}, {
-  title: 'Canonical string reduction',
-  author: 'Edsger W. Dijkstra',
-  url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-  likes: 12
-}, {
-  title: 'First class tests',
-  author: 'Robert C. Martin',
-  url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-  likes: 10
-}, {
-  title: 'TDD harms architecture',
-  author: 'Robert C. Martin',
-  url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-  likes: 0
-}, {
-  title: 'Type wars',
-  author: 'Robert C. Martin',
-  url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-  likes: 2
-}]
-
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
-  await blogObject.save()
+
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
 })
 
 test('blogs are returned as json', async () => {
@@ -56,7 +25,7 @@ test('blogs are returned as json', async () => {
 test('there are two blogs', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(2)
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
 test('the first blog is https://reactpatterns.com/', async () => {
@@ -64,6 +33,34 @@ test('the first blog is https://reactpatterns.com/', async () => {
 
   const urls = response.body.map(r => r.url)
   expect(urls).toContain('https://reactpatterns.com/')
+})
+
+test('check blog has id property', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].id).toBeDefined()
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'new test blog',
+    author: 'alice',
+    url: 'http://example.com',
+    likes: 20
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const urls = response.body.map(r => r.url)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  expect(urls).toContain('http://example.com')
 })
 
 afterAll(() => {
