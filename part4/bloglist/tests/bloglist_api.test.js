12adb5debe1d3c4cf7c8ddc31c36f1d2d6f83edf
Mo <alim0x@outlook.com>
2021-01-27 12:31:27 +0800

    complete Exercises 4.11.-4.12.

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -15,24 +15,26 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('there are two blogs', async () => {
-  const response = await api.get('/api/blogs')
+  test('there are blogs', async () => {
+    const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
 
-test('the first blog is https://reactpatterns.com/', async () => {
-  const response = await api.get('/api/blogs')
+  test('the first blog is https://reactpatterns.com/', async () => {
+    const response = await api.get('/api/blogs')
 
-  const urls = response.body.map(r => r.url)
-  expect(urls).toContain('https://reactpatterns.com/')
+    const urls = response.body.map(r => r.url)
+    expect(urls).toContain('https://reactpatterns.com/')
+  })
 })
 
 test('check blog has id property', async () => {
@@ -61,6 +63,38 @@ test('a valid blog can be added', async () => {
 
   expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
   expect(urls).toContain('http://example.com')
+})
+
+test('property likes equals 0 if request does not contain it', async () => {
+  const newBlog = {
+    title: 'test blog without likes',
+    author: 'bob',
+    url: 'http://example.com'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const blogWithoutLikes = response.body.filter(r => r.title === 'test blog without likes')
+  expect(blogWithoutLikes[0].likes).toBeDefined()
+  expect(blogWithoutLikes[0].likes).toEqual(0)
+})
+
+test('response 400 if post request does not have title and url', async () => {
+  const newBlog = {
+    author: 'bob',
+    likes: 10
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
 })
 
 afterAll(() => {
