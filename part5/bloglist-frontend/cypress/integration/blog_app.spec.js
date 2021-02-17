7afd82116cd84a1cc399574821720fccf20e802e
alim0x <alim0x@outlook.com>
2021-02-17 15:31:48 +0800

    complete Exercises 5.21.

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -36,7 +36,7 @@ describe('Blog app', function() {
     })
   })
 
-  describe.only('When logged in', function() {
+  describe('When logged in', function() {
     beforeEach(function() {
       cy.get('#username').type('example')
       cy.get('#password').type('secret')
@@ -64,6 +64,30 @@ describe('Blog app', function() {
       cy.contains('like').click()
 
       cy.contains('likes 1')
+    })
+
+    it.only('A blog can be deleted', function() {
+      cy.contains('new blog').click()
+      cy.get('#title').type('newblog-1')
+      cy.get('#author').type('newauthor-1')
+      cy.get('#url').type('newurl-1')
+      cy.get('#create-blog').click()
+
+      cy.wait(5000)
+
+      cy.get('button').contains('new blog').click()
+      cy.get('#title').type('newblog-2')
+      cy.get('#author').type('newauthor-2')
+      cy.get('#url').type('newurl-2')
+      cy.get('#create-blog').click()
+
+      cy.wait(5000)
+
+      cy.contains('newblog-1 newauthor-1').contains('view').click()
+      cy.get('button').contains('remove').click()
+
+      cy.get('.blog').should('not.contain', 'newblog-1')
+      cy.get('.blog').should('contain', 'newblog-2')
     })
   })
 })
