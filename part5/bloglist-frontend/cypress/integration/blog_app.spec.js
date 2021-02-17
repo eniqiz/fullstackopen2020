791a328db3e7f566ce19c0369b3f3d56a9ca8436
alim0x <alim0x@outlook.com>
2021-02-17 14:51:56 +0800

    complete Exercises 5.18.

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -11,7 +11,26 @@ describe('Blog app', function() {
   })
 
   it('Login form is shown', function() {
-      cy.visit('http://localhost:3000')
       cy.contains('Log in to application')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('example')
+      cy.get('#password').type('secret')
+      cy.get('#login-button').click()
+
+      cy.contains('Example logged-in')
+    })
+  
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('example')
+      cy.get('#password').type('123456')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -15,9 +15,9 @@ const LoginForm = ({
     <h2>Log in to application</h2>
     <Notification message={message} errorMessage={errorMessage}/>
     <form onSubmit={handleLogin}>
-      <div>username<input value={username} onChange={( { target } ) => setUsername(target.value)}/></div>
-      <div>password<input value={password} onChange={( { target } ) => setPassword(target.value)} type="password"/></div>
-      <button type="submit">login</button>
+      <div>username<input id='username' value={username} onChange={( { target } ) => setUsername(target.value)}/></div>
+      <div>password<input id='password' value={password} onChange={( { target } ) => setPassword(target.value)} type="password"/></div>
+      <button id='login-button' type="submit">login</button>
     </form>
   </div>
 )
