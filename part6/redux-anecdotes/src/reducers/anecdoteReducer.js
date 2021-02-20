2cc9b5391cfdb72c7b38165116222afd3c21e35a
Mo <alim0x@outlook.com>
2021-02-20 08:44:27 +0000

    complete Exercises 6.5.

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -30,7 +30,8 @@ const reducer = (state = initialState, action) => {
         ...anecdoteToVote,
         votes: anecdoteToVote.votes + 1
       }
-      return state.map(anecdote => anecdote.id !== id ? anecdote : votedAnecdote)
+      const newState = state.map(anecdote => anecdote.id !== id ? anecdote : votedAnecdote)
+      return newState.sort((a, b) => b.votes - a.votes)
     default:
       return state
   }
